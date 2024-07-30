ee5e37577782726395d8eac8e471875ac184059a
Ella Sun <133064894+grappeefruitt@users.noreply.github.com>
2024-07-29 20:49:15 -0400

    Add files via upload

---
+++ EDA Analysis_worldlayoffs.sql
@@ -0,0 +1,74 @@
+-- EDA Analysis
+
+SELECT * 
+FROM world_layoffs.layoffs_staging2;
+
+SELECT MAX(total_laid_off), MAX(percentage_laid_off)
+FROM layoffs_staging2;
+
+SELECT * 
+FROM world_layoffs.layoffs_staging2
+WHERE percentage_laid_off = 1
+ORDER BY total_laid_off DESC;
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT country, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC;
+
+SELECT MIN('DATE'),MAX('DATE')
+FROM layoffs_staging2;
+
+SELECT YEAR(date), SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY YEAR(date)
+ORDER BY 1 ASC;
+
+
+SELECT industry, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC;
+
+-- Companies with the most layoffs per year
+WITH Company_Year AS 
+(
+  SELECT company, YEAR(date) AS years, SUM(total_laid_off) AS total_laid_off
+  FROM layoffs_staging2
+  GROUP BY company, YEAR(date)
+)
+, Company_Year_Rank AS (
+  SELECT company, years, total_laid_off, DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS ranking
+  FROM Company_Year
+)
+SELECT company, years, total_laid_off, ranking
+FROM Company_Year_Rank
+WHERE ranking <= 3
+AND years IS NOT NULL
+ORDER BY years ASC, total_laid_off DESC;
+
+SELECT SUBSTRING(date,6,2) as month, SUM(total_laid_off) AS total_laid_off
+FROM layoffs_staging2
+GROUP BY 'month';
+
+SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
+FROM layoffs_staging2
+GROUP BY dates
+ORDER BY dates ASC;
+
+WITH DATE_CTE AS 
+(
+SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
+FROM layoffs_staging2
+GROUP BY dates
+ORDER BY dates ASC
+)
+SELECT dates, SUM(total_laid_off) OVER (ORDER BY dates ASC) as rolling_total_layoffs
+FROM DATE_CTE
+ORDER BY dates ASC;
+
